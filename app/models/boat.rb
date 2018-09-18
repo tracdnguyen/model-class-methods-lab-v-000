d4b6e501141e0076a2a45843d136c5bda31bfde7
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-18 23:29:49 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,7 +3,7 @@ class Boat < ActiveRecord::Base
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
 
-  def first_five
+  def self.first_five
 
   end
 end
