4d43fdec12fcf0786c1cda02d9132f090f9fd7e9
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-19 00:06:08 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,6 +24,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    includes(:classifications).where(classification: { name: 'Sailboat'})
+    includes(:classifications).where(classifications: { name: 'Sailboat'})
   end
 end
