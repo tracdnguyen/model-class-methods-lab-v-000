e867091d3a7cde3042b519f00e23a1020d8fe439
Trac Nguyen <nguyendtrac@gmail.com>
2018-09-19 00:26:07 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -4,4 +4,8 @@ class Captain < ActiveRecord::Base
   def self.catamaran_operators
     includes(boats: :classifications).where(classifications: { name: 'Catamaran'})
   end
+
+  def self.sailors
+
+  end
 end
